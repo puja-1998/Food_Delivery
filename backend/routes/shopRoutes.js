e025fe19fc2db97656c24aef15f10cc1b33487cb
Puja-1998 <ghodkepooja98@gmail.com>
2025-11-18 11:44:57 +0530

    item controller and routes is created for add and edit item for owner

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import isAuth from '../middlewares/isAuth.js';
 import { createEditShop } from '../controllers/shopController.js';
+import { upload } from '../middlewares/multer.js';
 
 
 const shopRouter = express.Router();
 
-shopRouter.get("/create-edit",isAuth,  createEditShop);
+shopRouter.get("/create-edit",isAuth, upload.single("image"),  createEditShop);
 
 
 export default shopRouter;
