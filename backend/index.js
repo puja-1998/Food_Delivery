d289557d7d73d0a3cd91c09b5c93db323474312a
Puja-1998 <ghodkepooja98@gmail.com>
2025-11-18 19:45:42 +0530

    created hook for getMyshop

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connextDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+
+import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
-
-import cors from 'cors';
+import shopRouter from './routes/shopRoutes.js';
+import itemRouter from './routes/itemRoutes.js';
 
 dotenv.config();
 
@@ -14,13 +17,15 @@ const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://localhost:5173",  // fronend request
     credentials:true
 }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/shop", shopRouter);
+app.use("/api/item", itemRouter);
 
 
 app.get("/", (req, res)=>{
@@ -32,7 +37,7 @@ app.listen(port, (err)=>{
         console.log("Unable to connect to the server");
     }
     console.log(`Server is running at http://localhost:${port}`);
-    connextDB();
+    connectDB();
 })
 
 
